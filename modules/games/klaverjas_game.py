d4e503223c2d6dad55be73f1f8f5deb80e7da3de
alex <alex@e-cube.nl>
2024-03-26 08:15:31 +0100

    Add newline

--- modules/games/klaverjas_game.py
+++ modules/games/klaverjas_game.py
@@ -383,7 +383,7 @@ class Klaverjas(BaseGame):
 
     def game_end_message(self):
         msg = "Klaverjas potje met seed {}\n".format(self.seed)
-        msg += "Vergelijken? Helaas, Ingrid kan dit (nog) niet. Val Alex lastig als je dit graag wil."
+        msg += "Vergelijken? Helaas, Ingrid kan dit (nog) niet. Val Alex lastig als je dit graag wil.\n"
         msg += "Round {}\n".format(len(self.round_lists))
         if self.p1.is_playing:
             msg += "Team 1: *{}*, *{}*\n".format(self.p1.name, self.p3.name)
